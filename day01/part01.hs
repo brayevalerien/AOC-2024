2c2107961725963e5ca324bb5b48a831e618065f
Valérien <valerien.brayee@gmail.com>
2024-12-02 16:18:07 +0100

    solved day 1 part 1 in Haskell

---
+++ day01/part01.hs
@@ -0,0 +1,19 @@
+import Data.List
+
+parse :: [String] -> ([Int], [Int])
+parse content = parse_aux content [] []
+    where
+        parse_aux :: [String] -> [Int] -> [Int] -> ([Int], [Int]) 
+        parse_aux [] left right = (left, right)
+        parse_aux (x:xs) left right =
+            let [l, r] = words x
+            in parse_aux xs ((read l):left) ((read r):right)
+    
+result :: [Int] -> [Int] -> Int
+-- There probably is a more elegant way to do it using folding
+result left right = sum $ map (\(l, r) -> abs (l-r)) (zip (sort left) (sort right))
+
+main = do
+    content <- readFile "day01/input.txt"
+    let (left, right) = parse (lines content)
+    putStrLn $ show (result left right)
